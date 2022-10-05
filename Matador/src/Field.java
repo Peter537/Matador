07dcad11bb6588e626dff167f10aa91ad89b6651
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-05 11:32:56 +0200

    Opdatering af Field klassen

--- Matador/src/Field.java
+++ Matador/src/Field.java
@@ -1,30 +1,58 @@
 public class Field {
 
-    private final int ID;
     private final String label;
+    private final int ID;
     private final int cost;
     private final int income;
     private final int seriesID;
-    private final Player owner;
+    private Player owner;
 
     public Field(int ID, String label, int cost, int income, int seriesID, Player owner) {
-        this.ID = ID;
         this.label = label;
+        this.ID = ID;
         this.cost = cost;
         this.income = income;
         this.seriesID = seriesID;
         this.owner = owner;
     }
 
+    public String onLand() {
+        return "Du er landet på " + ID + " " + label;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public int getID() {
+        return ID;
+    }
+
+    public int getCost() {
+        return cost;
+    }
+
+    public int getIncome() {
+        return income;
+    }
+
+    public int getSeriesID() {
+        return seriesID;
+    }
+
+    public Player getOwner() {
+        return owner;
+    }
+
+    public void setOwner(Player owner) {
+        this.owner = owner;
+    }
+
     @Override
     public String toString() {
         return "Field{" +
-                "ID=" + ID +
-                ", label='" + label + '\'' +
-                ", cost=" + cost +
-                ", income=" + income +
-                ", seriesID=" + seriesID +
-                ", owner=" + owner +
+                "ID='" + ID + '\'' +
+                ", label=" + label +
                 '}';
     }
 }
