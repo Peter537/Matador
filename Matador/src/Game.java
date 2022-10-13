c5f4cb7225decada35e13571f9b792d430b6f737
Peter537 <52176852+Peter537@users.noreply.github.com>
2022-10-13 10:12:37 +0200

    Update Game.java

--- Matador/src/Game.java
+++ Matador/src/Game.java
@@ -26,14 +26,7 @@ public class Game {
         // Load af felt data og bygge boardet
         //******************************************************
         String[] fieldData = fileIO.readBoardData();
-
-        /*TODO: tilføj en linie der minder om den lige oven over.
-           Her skal der dog loades Chance-kort data istedet for felt data (jvf. Task 2.d)
-         * */
-
         ArrayList<String> chanceData = fileIO.readChanceData();
-
-        /*TODO: ændr dette konstruktor kald til Board, sådan at  Chance-kort data kommer med som argument (jvf. Task 2.d) */
 
         board = new Board(fieldData, chanceData);
 
